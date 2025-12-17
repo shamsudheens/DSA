66904861e8403ad753b7e73e129297a3d39a891b
chechu776 <shameerchechu@gmail.com>
2025-12-17 10:22:10 +0530

    climbingladder

---
+++ climbingLadder.js
@@ -0,0 +1,17 @@
+var climbStairs = function(n) {
+    if (n <= 2) return n;
+
+    let prev1 = 1; 
+    let prev2 = 2; 
+
+    for (let i = 3; i <= n; i++) {
+        let curr = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = curr;
+    }
+
+    return prev2;
+};
+
+let n = 6;
+console.log(climbStairs(n));
