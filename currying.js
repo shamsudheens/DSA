02977922fe7f0747dbe18138e8fee1092b162943
chechu776 <shameerchechu@gmail.com>
2026-01-16 17:47:38 +0530

    currying

--- currying.js
+++ currying.js
@@ -1,3 +1,11 @@
-const ADD = a => b => c => a+b+c;
+// const ADD = a => b => c => a+b+c;
+
+function ADD(a){
+    return function (b){
+        return function (c){
+            return a+b+c;        
+        }
+    }
+}
 
 console.log(ADD(1)(2)(3));
