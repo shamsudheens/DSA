be481c12aece580bcf9e5baebbe360629e581970
chechu776 <shameerchechu@gmail.com>
2026-01-17 10:52:30 +0530

    done

---
+++ duplicate.js
@@ -0,0 +1,30 @@
+const duplicate = (arr) =>{
+    return arr.filter((e,index,arr)=>{
+        return arr.indexOf(e) !== index;
+    })
+}
+
+const findDuplicate = (arr) => {
+  return arr.filter((v, i, a) => a.indexOf(v) !== i)[0];
+};
+
+const Duplicate = (arr) => {
+  const seen = new Set();
+  const duplicates = new Set();
+
+  for (let num of arr) {
+    if (seen.has(num)) {
+      duplicates.add(num);
+    } else {
+      seen.add(num);
+    }
+  }
+
+  return [...duplicates];
+};
+
+const arr = [1,3,4,2,2,3];
+console.log(duplicate(arr))
+console.log(Duplicate(arr))
+console.log(findDuplicate(arr))
+
---
+++ outputguess.js
@@ -0,0 +1,4 @@
+console.log([]+[]);
+console.log("10" + 10 - 10);
+console.log(typeof function(){})
+console.log([] == ![]);
