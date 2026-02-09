e89140a40bcb149ae7d0e7708cb673e9f71d9ae4
chechu776 <shameerchechu@gmail.com>
2026-02-09 10:42:19 +0530

    done largest odd number

---
+++ largestOddNumber.js
@@ -0,0 +1,11 @@
+const largestOdd = (num) => {
+    for (let i = num.length - 1; i >= 0; i--) {
+        if (num[i] % 2 !== 0) {
+            return num.slice(0, i + 1);
+        }
+    }
+    return "";
+};
+
+const num = "986464";
+console.log(largestOdd(num)); 
